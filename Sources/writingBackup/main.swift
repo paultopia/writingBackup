55445d913b05bbe608029e73e03a9ba5338979d4
Paul Gowder <paul.gowder@gmail.com>
2020-04-08 11:09:44 -0500

    clear unused code

--- Sources/writingBackup/main.swift
+++ Sources/writingBackup/main.swift
@@ -1,27 +1,5 @@
 import writingBackupCore
-import Foundation
-
-// let inputFile = readConfig()
-
-// try PanDocConvert (
-  // sourceFile: inputFile,
-  // resultFile: "output_test.md",
-  // bibFile: "pgtest.json")
-
-// let foo = try combineFiles(files: ["testdoc.md", "secondfile.md"])
-
-// print(foo)
-
-
-//resultFile: "~/writingbackup_output_test.md", WORKS
-
-//print("Success!")
 
 let configFile = BackupConfig(from: "config.toml")
 try configFile.convert()
-// print(configFile.inFiles)
-// print(configFile.outFile)
-// print(configFile.bibFile)
-// print(configFile.combinedTempString)
 
-// makeTempFile(with: "test")
--- Sources/writingBackupCore/writingBackupCore.swift
+++ Sources/writingBackupCore/writingBackupCore.swift
@@ -19,6 +19,7 @@ public struct BackupConfig {
         let combined = try! combineFiles(from: self.inFiles)
         let tempFile = makeTempFile(with: combined)
         var args: [String]
+        let env = [("PATH", "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin")]
         if let references = self.bibFile {
             args = [tempFile,
                         "-f", "markdown",
@@ -35,7 +36,7 @@ public struct BackupConfig {
 
         let proc = try SysProcess("pandoc",
                                   args: args,
-                                  env: [("PATH", "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin")])
+                                  env: env)
         let res = try proc.wait(hang: true)
         if res != 0 {
             let s = try proc.stderr?.readString() ?? "Unknown Error"
@@ -44,29 +45,5 @@ public struct BackupConfig {
         print("successfully backed up \(self.inFiles) to \(self.outFile)!")
         cleanUp(tempFile)
     }
-}
-
-
-
-
-
-public func PanDocConvert(sourceFile: String, resultFile: String, bibFile: String) throws {
-    let proc = try SysProcess("pandoc",
-                              args: [sourceFile,
-                                     "-f", "markdown",
-                                     "--filter", "pandoc-citeproc",
-                                     "-t", "markdown-raw_html-citations-native_divs-native_spans",
-                                     "--bibliography", bibFile,
-                                     "-o", NSString(string: resultFile).expandingTildeInPath],
-                              env: [("PATH", "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin")])
-    let res = try proc.wait(hang: true)
-    if res != 0 {
-        let s = try proc.stderr?.readString() ?? "Unknown Error"
-        throw PerfectError.systemError(Int32(res), s)
-    }
-}
-
-public func runTests() {
-    print("running")
 }
 
