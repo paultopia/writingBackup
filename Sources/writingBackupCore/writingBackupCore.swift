3dfbe2417a7457f2802f05bd34bdc6f6f6b7b2be
Paul Gowder <paultopia@users.noreply.github.com>
2020-04-16 21:55:05 -0500

    make run result discardable

--- Sources/writingBackupCore/writingBackupCore.swift
+++ Sources/writingBackupCore/writingBackupCore.swift
@@ -92,7 +92,7 @@ struct BackupConfig {
     }
 }
 
-public func runBackup() throws -> (String, String) {
+@discardableResult public func runBackup() throws -> (String, String) {
     let configFile = try BackupConfig(from: "backup.toml") 
     let outputData = try configFile.convert()
     return outputData // returning just for testing purposes.
